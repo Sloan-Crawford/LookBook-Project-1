60b5d1318e912d40c1d4e11373753f14a3a8836f
Sloan-Crawford <sloan.crawford@gmail.com>
2022-05-27 10:32:07 -0400

    fix links and update styling

--- SearchString.js
+++ SearchString.js
@@ -160,10 +160,3 @@ function performMark() {
  };
 
 }
-
-// const links = document.querySelector('nav-links');
-// const navBtnDOM = document.querySelector('nav-btn');
-
-// navBtnDOM.addEventListener('click', () => {
-//   links.classList.toggle('show-links')
-// });
